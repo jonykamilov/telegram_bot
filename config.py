6deb38d4aefb75cd0b2470bc8e549bcd5d7fe1c3
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2026-02-19 17:33:39 +0500

    token sozlandi

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 # config.py
-BOT_TOKEN = "8480041804:AAEoT13D4tqrbQ6A4C3wTuSULSy0mfHxvao"  # BotFather dan olingan tokenni qo'ying
+import os
+#BOT_TOKEN = "8480041804:AAEoT13D4tqrbQ6A4C3wTuSULSy0mfHxvao"  # BotFather dan olingan tokenni qo'ying
+BOT_TOKEN = os.environ.get('BOT_TOKEN')  # Yangi qator: tokenni Railway'dan ol
+
 GROUP_LINK = "https://t.me/megamir_xorazmm"  # Guruh linkingiz
 CHANNEL_ID = "@megamir_xorazmm"  # Kanal yoki guruh username i
 
